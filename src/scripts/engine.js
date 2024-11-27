b595da7dd627fb539c2d62329cdc359f89272eef
ErikaFiatkoski <erika.fiatkoski06@gmail.com>
2024-11-26 22:03:27 -0300

    Criacao do readme

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,58 +1,60 @@
 const emojis = [
-  "🐱",
-  "🐱",
-  "🦝",
-  "🦝",
-  "🦊",
-  "🦊",
-  "🐶",
-  "🐶",
-  "🐵",
-  "🐵",
-  "🦁",
-  "🦁",
-  "🐯",
-  "🐯",
-  "🐮",
-  "🐮",
-];
-let openCards = [];
+    "😺",
+    "😺",
+    "🐵",
+    "🐵",
+    "🐺",
+    "🐺",
+    "🦊",
+    "🦊",
+    "🦁",
+    "🦁",
+    "🐹",
+    "🐹",
+    "🐸",
+    "🐸",
+    "🐼",
+    "🐼",
+
+]; // Array que guarda todos os emojis usados no jogo
+
+let openCards = []; // Array que guarda as cartas que "foram viradas/abertas"
+
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for (let i = 0; i < emojis.length; i++) {
-  let box = document.createElement("div");
-  box.className = "item";
-  box.innerHTML = shuffleEmojis[i];
-  box.onclick = handleClick;
-  document.querySelector(".game").appendChild(box);
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
 }
 
-function handleClick() {
-  if (openCards.length < 2) {
-    this.classList.add("boxOpen");
-    openCards.push(this);
-  }
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
 
-  if (openCards.length == 2) {
-    setTimeout(checkMatch, 500);
-  }
-
-  console.log(openCards);
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
 }
 
-function checkMatch() {
-  if (openCards[0].innerHTML === openCards[1].innerHTML) {
-    openCards[0].classList.add("boxMatch");
-    openCards[1].classList.add("boxMatch");
-  } else {
-    openCards[0].classList.remove("boxOpen");
-    openCards[1].classList.remove("boxOpen");
-  }
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    } else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
 
-  openCards = [];
+    openCards = [];
 
-  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
-    alert("Você venceu !");
-  }
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Você venceu!");
+    }
 }
---
+++ src/styles/README.md
@@ -0,0 +1,12 @@
+# JOGO DA MEMÓRIA COM EMOJIS 😺
+
+Neste projeto, desenvolvido utilizando HTML, CSS e Javascript, foi desenvolvido um jogo da memória contendo 8 pares de cartas.
+
+### Modo de jogar:
+
+- Basta selecionar duas cartas por vez, com o objetivo de encontrar os pares. Se as cartas selecionadas formarem um par, você pontua. Caso contrário, as cartas voltam a seu estado inicial.
+- O jogo termina quando todos os pares forem encontrados.
+
+<hr>
+
+Projeto ofertado pela [DIO - Digital Innovation One](https://www.dio.me/)
